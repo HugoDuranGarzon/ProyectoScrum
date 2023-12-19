9dbb066282bcd23292e931a622fc255f6a9834f3
Administrador <Administrador@2DAM01>
2023-12-19 18:14:50 +0100

    centrar pagina principal+

--- src/vistas/FrmTrabajadores.java
+++ src/vistas/FrmTrabajadores.java
@@ -21,6 +21,7 @@ public class FrmTrabajadores extends javax.swing.JFrame {
      */
     public FrmTrabajadores() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
